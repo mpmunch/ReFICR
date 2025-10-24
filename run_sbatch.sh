e77a1cebc270230f36601d2f4e205ef63d12c6fa
Mathias Pallesen Munch <mpmunch@gmail.com>
2025-10-24 10:54:45 +0200

    sbatch script

---
+++ run_sbatch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH --job-name=reficr_training
+#SBATCH --output=reficr_training.out
+#SBATCH --error=reficr_training.err
+#SBATCH --mem=24G
+#SBATCH --cpus-per-task=15
+#SBATCH --gres=gpu:1
+#SBATCH --time=02:00:00
+
+# Run script in container
+# singularity exec --nv /ceph/project/python/python_3.10.sif bash run.sh
+singularity exec --nv p9-reficr_latest.sif bash run.sh
